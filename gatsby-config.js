fc2f7c600a11d7a347d3654fab97a636089cbea6
nguyendinhtrieu <nguyendinhtrieu365@gmail.com>
2023-06-27 14:30:45 +0700

    getting-started/part-4 - Task: Use GraphiQL to build the query

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `learn-gatsby`,
+    title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
